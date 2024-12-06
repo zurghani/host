af02b97acfd4e2539585d25636d690ffd1141f2f
= <abdozurghani@gmail.com>
2024-12-06 09:01:51 -0500

    artifact added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true, // Standalone component
+  schemas: [CUSTOM_ELEMENTS_SCHEMA], // Allow custom elements
 })
 export class AppComponent {
   title = 'host';
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import '../../dist/artifact'; // Adjust the path as needed
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
